35ea87bf4fd8bcaae7c3338a59a3e176fe7b090f
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-02-24 15:41:33 +0500

    Решение седьмой задачи

--- mars.py
+++ mars.py
@@ -235,5 +235,32 @@ def chosen_planet(planet_name):
                     </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def res(nickname, level, rating):
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}">
+                            <title>Варианты выбора</title>
+                          </head>
+                          <body>
+                            <h2>Результаты отбора</h2>
+                            <h3>Претендента на участие в миссии {nickname}:</h3>
+                            <div class="alert alert-success" role="alert">
+                              Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating}! 
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                              Желаем удачи!
+                            </div>
+                          </body>
+                        </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
