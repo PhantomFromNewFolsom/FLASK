36c1733a008755f4f8504e566d05837e500d2ef1
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-02-25 11:56:09 +0500

    Решение второй задачи нового урока

--- shablony.py
+++ shablony.py
@@ -9,5 +9,10 @@ def home():
     return render_template('base.html', title='Домашняя страница')
 
 
+@app.route('/training/<prof>')
+def tran(prof):
+    return render_template('train.html', title='Тренировка', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
