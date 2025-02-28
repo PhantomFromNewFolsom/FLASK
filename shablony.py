053b91358d1b603dbe40553f1492ec5868f2f3b8
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-02-28 15:57:38 +0500

    Решение пятой задачи нового урока

--- shablony.py
+++ shablony.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    cap_name = StringField('Id капитана', validators=[DataRequired()])
+    cap_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -27,6 +41,19 @@ def ans():
          'profession': 'космодесантник', 'sex': 'male',
          'motivation': 'Я люблю красный! Готов к встрече с враждебными марсианами.', 'ready': True}
     return render_template('auto_answer.html', title=d['title'], dictn=d)
+
+
+@app.route('/success')
+def suc():
+    return render_template('suc.html', title='Успех')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('double_defend.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
